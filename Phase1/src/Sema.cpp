87e2877fcedd4bf4117dd25befa4c9a205010fa1
Shayan Kebriti <shayankebriti@gmail.com>
2023-12-14 01:13:37 +0330

    Fix

--- Phase1/src/Sema.cpp
+++ Phase1/src/Sema.cpp
@@ -158,16 +158,18 @@ public:
   virtual void visit(If &Node) override {
     llvm::errs() << "IF" << "\n";
     Conditions *conds = Node.getConds();
-    Else *ElseBranch = Node.getElse();
 
     conds->accept(*this);
 
     for (llvm::SmallVector<Assign *>::const_iterator I = Node.AssignmentsBegin(), E = Node.AssignmentsEnd(); I != E; ++I) {
         (*I)->accept(*this);
     }
+    
     for (llvm::SmallVector<Elif *>::const_iterator I = Node.ElifsBegin(), E = Node.ElifsEnd(); I != E; ++I) {
         (*I)->accept(*this);
     }
+
+    Else *ElseBranch = Node.getElse();
 
     if(ElseBranch) ElseBranch->accept(*this);
 
