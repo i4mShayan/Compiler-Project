a5a398419e407177de7f69bd8361bd5dfdb099a3
Amin Rafiee <93943199+aminrfe@users.noreply.github.com>
2023-12-14 02:28:51 +0330

    Fix Declare

--- Phase1/src/CodeGen.cpp
+++ Phase1/src/CodeGen.cpp
@@ -87,6 +87,7 @@ namespace
       // }
     
     };
+
     virtual void visit(Assign &Node) override
     {
       // Visit the right-hand side of the assignment and get its value.
@@ -98,114 +99,114 @@ namespace
 
       switch (Node.getAssignmentOP())
       {
-      case Assign::EqualAssign:
-      {
-        // Create a store instruction to assign the value to the variable.
-        Builder.CreateStore(val, nameMap[varName]);
-
-        // Create a function type for the "gsm_write" function.
-        FunctionType *CalcWriteFnTy = FunctionType::get(VoidTy, {Int32Ty}, false);
-
-        // Create a function declaration for the "gsm_write" function.
-        Function *CalcWriteFn = Function::Create(CalcWriteFnTy, GlobalValue::ExternalLinkage, "gsm_write", M);
-
-        // Create a call instruction to invoke the "gsm_write" function with the value.
-        CallInst *Call = Builder.CreateCall(CalcWriteFnTy, CalcWriteFn, {val});
-
-        break;
-      }
-      case Assign::PlusAssign :
-      {
-        // Create a load instruction to get the current value of the variable.
-        Value *oldVal = Builder.CreateLoad(Int32Ty, nameMap[varName]);
-
-        // Create an add instruction to add the old value and the new value.
-        Value *newVal = Builder.CreateNSWAdd(oldVal, val);
-
-        // Create a store instruction to assign the new value to the variable.
-        Builder.CreateStore(newVal, nameMap[varName]);
-
-        // Create a function type for the "gsm_write" function.
-        FunctionType *CalcWriteFnTy2 = FunctionType::get(VoidTy, {Int32Ty}, false);
-
-        // Create a function declaration for the "gsm_write" function.
-        Function *CalcWriteFn2 = Function::Create(CalcWriteFnTy2, GlobalValue::ExternalLinkage, "gsm_write", M);
-
-        // Create a call instruction to invoke the "gsm_write" function with the new value.
-        CallInst *Call2 = Builder.CreateCall(CalcWriteFnTy2, CalcWriteFn2, {newVal});
-
-        break;
-      }
-      case Assign::MinusAssign:
-      {
-        // Create a load instruction to get the current value of the variable.
-        Value *oldVal2 = Builder.CreateLoad(Int32Ty, nameMap[varName]);
-
-        // Create a sub instruction to subtract the old value and the new value.
-        Value *newVal2 = Builder.CreateNSWSub(oldVal2, val);
-
-        // Create a store instruction to assign the new value to the variable.
-        Builder.CreateStore(newVal2, nameMap[varName]);
-
-        // Create a function type for the "gsm_write" function.
-        FunctionType *CalcWriteFnTy3 = FunctionType::get(VoidTy, {Int32Ty}, false);
-
-        // Create a function declaration for the "gsm_write" function.
-        Function *CalcWriteFn3 = Function::Create(CalcWriteFnTy3, GlobalValue::ExternalLinkage, "gsm_write", M);
-
-        // Create a call instruction to invoke the "gsm_write" function with the new value.
-        CallInst *Call3 = Builder.CreateCall(CalcWriteFnTy3, CalcWriteFn3, {newVal2});
-
-        break;
-      }
-      case Assign::MulAssign:
+        case Assign::EqualAssign:
         {
-        // Create a load instruction to get the current value of the variable.
-        Value *oldVal3 = Builder.CreateLoad(Int32Ty, nameMap[varName]);
+          // Create a store instruction to assign the value to the variable.
+          Builder.CreateStore(val, nameMap[varName]);
 
-        // Create a mul instruction to multiply the old value and the new value.
-        Value *newVal3 = Builder.CreateNSWMul(oldVal3, val);
+          // Create a function type for the "gsm_write" function.
+          FunctionType *CalcWriteFnTy = FunctionType::get(VoidTy, {Int32Ty}, false);
 
-        // Create a store instruction to assign the new value to the variable.
-        Builder.CreateStore(newVal3, nameMap[varName]);
+          // Create a function declaration for the "gsm_write" function.
+          Function *CalcWriteFn = Function::Create(CalcWriteFnTy, GlobalValue::ExternalLinkage, "gsm_write", M);
 
-        // Create a function type for the "gsm_write" function.
-        FunctionType *CalcWriteFnTy4 = FunctionType::get(VoidTy, {Int32Ty}, false);
+          // Create a call instruction to invoke the "gsm_write" function with the value.
+          CallInst *Call = Builder.CreateCall(CalcWriteFnTy, CalcWriteFn, {val});
 
-        // Create a function declaration for the "gsm_write" function.
-        Function *CalcWriteFn4 = Function::Create(CalcWriteFnTy4, GlobalValue::ExternalLinkage, "gsm_write", M);
-
-        // Create a call instruction to invoke the "gsm_write" function with the new value.
-        CallInst *Call4 = Builder.CreateCall(CalcWriteFnTy4, CalcWriteFn4, {newVal3});
-
-        break;
+          break;
         }
-      case Assign::DivAssign:
-      {
-        // Create a load instruction to get the current value of the variable.
-        Value *oldVal4 = Builder.CreateLoad(Int32Ty, nameMap[varName]);
-        // Create a div instruction to divide the old value and the new value.
-        Value *newVal4 = Builder.CreateSDiv(oldVal4, val);
-        // Create a store instruction to assign the new value to the variable.
-        Builder.CreateStore(newVal4, nameMap[varName]);
-        // Create a function type for the "gsm_write" function.
-        FunctionType *CalcWriteFnTy5 = FunctionType::get(VoidTy, {Int32Ty}, false);
-        // Create a function declaration for the "gsm_write" function.
-        Function *CalcWriteFn5 = Function::Create(CalcWriteFnTy5, GlobalValue::ExternalLinkage, "gsm_write", M);
-        // Create a call instruction to invoke the "gsm_write" function with the new value.
-        CallInst *Call5 = Builder.CreateCall(CalcWriteFnTy5, CalcWriteFn5, {newVal4});
-        break;
-      }
-      case Assign::ModAssign:
-      {
-        Value *oldVal5 = Builder.CreateLoad(Int32Ty, nameMap[varName]);
-        Value *newVal5 = Builder.CreateSRem(oldVal5, val);
-        Builder.CreateStore(newVal5, nameMap[varName]);
-        FunctionType *CalcWriteFnTy6 = FunctionType::get(VoidTy, {Int32Ty}, false);
-        Function *CalcWriteFn6 = Function::Create(CalcWriteFnTy6, GlobalValue::ExternalLinkage, "gsm_write", M);
-        CallInst *Call6 = Builder.CreateCall(CalcWriteFnTy6, CalcWriteFn6, {newVal5});
-        break;
-      }
+        case Assign::PlusAssign :
+        {
+          // Create a load instruction to get the current value of the variable.
+          Value *oldVal = Builder.CreateLoad(Int32Ty, nameMap[varName]);
+
+          // Create an add instruction to add the old value and the new value.
+          Value *newVal = Builder.CreateNSWAdd(oldVal, val);
+
+          // Create a store instruction to assign the new value to the variable.
+          Builder.CreateStore(newVal, nameMap[varName]);
+
+          // Create a function type for the "gsm_write" function.
+          FunctionType *CalcWriteFnTy2 = FunctionType::get(VoidTy, {Int32Ty}, false);
+
+          // Create a function declaration for the "gsm_write" function.
+          Function *CalcWriteFn2 = Function::Create(CalcWriteFnTy2, GlobalValue::ExternalLinkage, "gsm_write", M);
+
+          // Create a call instruction to invoke the "gsm_write" function with the new value.
+          CallInst *Call2 = Builder.CreateCall(CalcWriteFnTy2, CalcWriteFn2, {newVal});
+
+          break;
+        }
+        case Assign::MinusAssign:
+        {
+          // Create a load instruction to get the current value of the variable.
+          Value *oldVal2 = Builder.CreateLoad(Int32Ty, nameMap[varName]);
+
+          // Create a sub instruction to subtract the old value and the new value.
+          Value *newVal2 = Builder.CreateNSWSub(oldVal2, val);
+
+          // Create a store instruction to assign the new value to the variable.
+          Builder.CreateStore(newVal2, nameMap[varName]);
+
+          // Create a function type for the "gsm_write" function.
+          FunctionType *CalcWriteFnTy3 = FunctionType::get(VoidTy, {Int32Ty}, false);
+
+          // Create a function declaration for the "gsm_write" function.
+          Function *CalcWriteFn3 = Function::Create(CalcWriteFnTy3, GlobalValue::ExternalLinkage, "gsm_write", M);
+
+          // Create a call instruction to invoke the "gsm_write" function with the new value.
+          CallInst *Call3 = Builder.CreateCall(CalcWriteFnTy3, CalcWriteFn3, {newVal2});
+
+          break;
+        }
+        case Assign::MulAssign:
+        {
+          // Create a load instruction to get the current value of the variable.
+          Value *oldVal3 = Builder.CreateLoad(Int32Ty, nameMap[varName]);
+
+          // Create a mul instruction to multiply the old value and the new value.
+          Value *newVal3 = Builder.CreateNSWMul(oldVal3, val);
+
+          // Create a store instruction to assign the new value to the variable.
+          Builder.CreateStore(newVal3, nameMap[varName]);
+
+          // Create a function type for the "gsm_write" function.
+          FunctionType *CalcWriteFnTy4 = FunctionType::get(VoidTy, {Int32Ty}, false);
+
+          // Create a function declaration for the "gsm_write" function.
+          Function *CalcWriteFn4 = Function::Create(CalcWriteFnTy4, GlobalValue::ExternalLinkage, "gsm_write", M);
+
+          // Create a call instruction to invoke the "gsm_write" function with the new value.
+          CallInst *Call4 = Builder.CreateCall(CalcWriteFnTy4, CalcWriteFn4, {newVal3});
+
+          break;
+        }
+        case Assign::DivAssign:
+        {
+          // Create a load instruction to get the current value of the variable.
+          Value *oldVal4 = Builder.CreateLoad(Int32Ty, nameMap[varName]);
+          // Create a div instruction to divide the old value and the new value.
+          Value *newVal4 = Builder.CreateSDiv(oldVal4, val);
+          // Create a store instruction to assign the new value to the variable.
+          Builder.CreateStore(newVal4, nameMap[varName]);
+          // Create a function type for the "gsm_write" function.
+          FunctionType *CalcWriteFnTy5 = FunctionType::get(VoidTy, {Int32Ty}, false);
+          // Create a function declaration for the "gsm_write" function.
+          Function *CalcWriteFn5 = Function::Create(CalcWriteFnTy5, GlobalValue::ExternalLinkage, "gsm_write", M);
+          // Create a call instruction to invoke the "gsm_write" function with the new value.
+          CallInst *Call5 = Builder.CreateCall(CalcWriteFnTy5, CalcWriteFn5, {newVal4});
+          break;
+        }
+        case Assign::ModAssign:
+        {
+          Value *oldVal5 = Builder.CreateLoad(Int32Ty, nameMap[varName]);
+          Value *newVal5 = Builder.CreateSRem(oldVal5, val);
+          Builder.CreateStore(newVal5, nameMap[varName]);
+          FunctionType *CalcWriteFnTy6 = FunctionType::get(VoidTy, {Int32Ty}, false);
+          Function *CalcWriteFn6 = Function::Create(CalcWriteFnTy6, GlobalValue::ExternalLinkage, "gsm_write", M);
+          CallInst *Call6 = Builder.CreateCall(CalcWriteFnTy6, CalcWriteFn6, {newVal5});
+          break;
+        }
       }
     };
 
@@ -260,7 +261,7 @@ namespace
       }
       case Expr::Pow:
       {
-        Final *right_final = Right.getLeft();
+        Final *right_final = Right->getLeft();
         int intval;
         intval = right_final->getVal().getAsInteger(10, intval);
         if (intval == 0)
@@ -269,7 +270,7 @@ namespace
         }
         else
         {
-          Final *temp = new Final(Left.getKind(), Left.getVal());
+          Final *temp = new Final(Left->getKind(), Left->getVal());
           for (int i = 1; i < intval; i++)
           {
             Left = Builder.CreateNSWMul(Left, temp);
@@ -286,31 +287,26 @@ namespace
         V = Builder.CreateNSWSub(Left, mul);
         break;
       }
+      }
     };
 
     virtual void visit(Declare &Node) override
     {
       Value *val = nullptr;
 
-      // Iterate over the variables declared in the declaration statement.
-      for (auto I = Node.VarsBegin(), g = Node.ExprsBegin(), E = Node.VarsEnd(); I != E; ++I, ++g)
+      L = llvm::SmallVector<Expr *>::const_iterator I = Node.ExprsBegin();
+      R = llvm::SmallVector<Expr *>::const_iterator E = Node.ExprsEnd();
+      for (llvm::SmallVector<llvm::StringRef, 8>::const_iterator I = Node.VarsBegin(), E = Node.VarsEnd(); I != E; ++I)
       {
         StringRef Var = *I;
-
-        // Create an alloca instruction to allocate memory for the variable.
         nameMap[Var] = Builder.CreateAlloca(Int32Ty);
 
-        if (g)
+        if (L != E)
         {
-          // If there is an expression provided, visit it and get its value.
-          g->accept(*this);
+          (*L)->accept(*this);
           val = V;
-        }
-
-        // Store the initial value (if any) in the variable's memory location.
-        if (val != nullptr)
-        {
           Builder.CreateStore(val, nameMap[Var]);
+          ++L;
         }
         else
         {
@@ -319,7 +315,7 @@ namespace
       }
     };
 
-    virtula void visit(Condition &Node) override
+    virtual void visit(Condition &Node) override
     {
       Node.getLeft()->accept(*this);
       Value *Left = V;
@@ -354,29 +350,29 @@ namespace
 
     virtual void visit(If &Node) override
     {
-      llvm::BasicBlock* IfCond = llvm::BasicBlock::Create(M->getContext(), "if.cond", MainFn);
-      llvm::BasicBlock* IfBody = llvm::BasicBlock::Create(M->getContext(), "if.body", MainFn);
-      llvm::BasicBlock* ElseBody = llvm::BasicBlock::Create(M->getContext(), "else.body", MainFn);
+      // llvm::BasicBlock* IfCond = llvm::BasicBlock::Create(M->getContext(), "if.cond", MainFn);
+      // llvm::BasicBlock* IfBody = llvm::BasicBlock::Create(M->getContext(), "if.body", MainFn);
+      // llvm::BasicBlock* ElseBody = llvm::BasicBlock::Create(M->getContext(), "else.body", MainFn);
 
-      Builder.CreateBr(IfCond);
-      Builder.SetInsertPoint(IfCond);
-      Node.getConditions()->accept(*this);
-      Value* Cond = V;
-      Builder.CreateCondBr(Cond, IfBody, ElseBody);
-      Builder.SetInsertPoint(IfBody);
+      // Builder.CreateBr(IfCond);
+      // Builder.SetInsertPoint(IfCond);
+      // Node.getConditions()->accept(*this);
+      // Value* Cond = V;
+      // Builder.CreateCondBr(Cond, IfBody, ElseBody);
+      // Builder.SetInsertPoint(IfBody);
 
-      for (auto I = Node.AssignmentsBegin(), E = Node.AssignmentsEnd(); I != E; ++I)
-      {
-          (*I)->accept(*this);
-      }
-      Builder.CreateBr(ElseBody);
+      // for (auto I = Node.AssignmentsBegin(), E = Node.AssignmentsEnd(); I != E; ++I)
+      // {
+      //     (*I)->accept(*this);
+      // }
+      // Builder.CreateBr(ElseBody);
 
-      Builder.SetInsertPoint(ElseBody);
+      // Builder.SetInsertPoint(ElseBody);
 
-      for (auto I = Node.getElse().AssignmentsBegin(), E = Node.getElse().AssignmentsEnd(); I != E; ++I)
-      {
-          (*I)->accept(*this);
-      }
+      // for (auto I = Node.getElse()->AssignmentsBegin(), E = Node.getElse()->AssignmentsEnd(); I != E; ++I)
+      // {
+      //     (*I)->accept(*this);
+      // }
       
     };
 
@@ -393,24 +389,24 @@ namespace
 
     virtual void visit(Loop &Node) override
     {
-      llvm::BasicBlock* LoopCond = llvm::BasicBlock::Create(M->getContext(), "loop.cond", MainFn);
-      llvm::BasicBlock* LoopBody = llvm::BasicBlock::Create(M->getContext(), "loop.body", MainFn);
-      llvm::BasicBlock* AfterLoop = llvm::BasicBlock::Create(M->getContext(), "after.loop", MainFn);
+      // llvm::BasicBlock* LoopCond = llvm::BasicBlock::Create(M->getContext(), "loop.cond", MainFn);
+      // llvm::BasicBlock* LoopBody = llvm::BasicBlock::Create(M->getContext(), "loop.body", MainFn);
+      // llvm::BasicBlock* AfterLoop = llvm::BasicBlock::Create(M->getContext(), "after.loop", MainFn);
 
-      Builder.CreateBr(LoopCond); 
-      Builder.SetInsertPoint(LoopCond); 
-      Node.getConditions()->accept(*this); 
-      Value* Cond = V; 
-      Builder.CreateCondBr(Cond, LoopBody, AfterLoop); 
-      Builder.SetInsertPoint(LoopBody); 
+      // Builder.CreateBr(LoopCond); 
+      // Builder.SetInsertPoint(LoopCond); 
+      // Node.getConds()->accept(*this); 
+      // Value* Cond = V; 
+      // Builder.CreateCondBr(Cond, LoopBody, AfterLoop); 
+      // Builder.SetInsertPoint(LoopBody); 
       
-      for (auto I = Node.AssignmentsBegin(), E = Node.AssignmentsEnd(); I != E; ++I) 
-      {
-          (*I)->accept(*this); 
-      }
-      Builder.CreateBr(LoopCond); 
+      // for (auto I = Node.AssignmentsBegin(), E = Node.AssignmentsEnd(); I != E; ++I) 
+      // {
+      //     (*I)->accept(*this); 
+      // }
+      // Builder.CreateBr(LoopCond); 
 
-      Builder.SetInsertPoint(AfterLoop);
+      // Builder.SetInsertPoint(AfterLoop);
 
     };
   };
