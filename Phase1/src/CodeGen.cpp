0870da6d6f6bc094067b8f80ce07456da0b2cdf7
Amin Rafiee <93943199+aminrfe@users.noreply.github.com>
2023-12-14 15:37:11 +0330

    Test

--- Phase1/src/CodeGen.cpp
+++ Phase1/src/CodeGen.cpp
@@ -412,9 +412,9 @@ namespace ns
 
     virtual void visit(Loop &Node) override
     {
-      llvm::BasicBlock* LoopCond = llvm::BasicBlock::Create(M->getContext(), "loop.cond", MainFn);
-      llvm::BasicBlock* LoopBody = llvm::BasicBlock::Create(M->getContext(), "loop.body", MainFn);
-      llvm::BasicBlock* AfterLoop = llvm::BasicBlock::Create(M->getContext(), "after.loop", MainFn);
+      llvm::BasicBlock* LoopCond = llvm::BasicBlock::Create(M->getContext(), "loop_cond", MainFn);
+      llvm::BasicBlock* LoopBody = llvm::BasicBlock::Create(M->getContext(), "loop_body", MainFn);
+      llvm::BasicBlock* AfterLoop = llvm::BasicBlock::Create(M->getContext(), "after_loop", MainFn);
 
       Builder.CreateBr(LoopCond); 
       Builder.SetInsertPoint(LoopCond); 
