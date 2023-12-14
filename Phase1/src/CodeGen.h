683e26134553b5ec3f663b415ff116664778ec35
Shayan Kebriti <shayankebriti@gmail.com>
2023-12-14 14:17:13 +0330

    Make the CodeGen Runnable

--- Phase1/src/CodeGen.h
+++ Phase1/src/CodeGen.h
@@ -1,12 +1,12 @@
-// #ifndef CODEGEN_H
-// #define CODEGEN_H
+#ifndef CODEGEN_H
+#define CODEGEN_H
 
-// #include "AST.h"
+#include "AST.h"
 
-// class CodeGen
-// {
-// public:
-//  void compile(AST *Tree);
+class CodeGen
+{
+public:
+ void compile(AST *Tree);
 
-// };
-// #endif
+};
+#endif
